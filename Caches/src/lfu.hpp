dabb18c2af5d2d626d04ab10458a5709cb848d39
Islam <iragimov@phystech.edu>
2023-11-06 11:09:17 +0000

    [Caches][LFU] Templated class; slow data obtaining
    Added templated parameter to LFU::Elem struct
    Added function parameter obtaining values from id

--- Caches/src/lfu.hpp
+++ Caches/src/lfu.hpp
@@ -4,12 +4,27 @@
 #include <list>
 #include <stdlib.h>
 #include <unordered_map>
+#include <functional>
 
 namespace caches {
 
+namespace dflt {
+
+using DataType = double;
+
+double dataGetter(size_t id) {
+  double valueFromId = static_cast<double>(id);
+  return valueFromId;
+}
+
+} // dflt
+
+template <typename Data = dflt::DataType>
 class LFU final {
 public:
-  explicit LFU(size_t size): sz_(size), minFreq(0) {}
+  explicit LFU(size_t size,
+               std::function<Data(size_t)> dataGetter = dflt::dataGetter):
+    sz_(size), valueFromIdFunc(dataGetter) {}
 
 public:
   void dump(std::ostream& OS) {
@@ -49,7 +64,7 @@ private:
 
   void InsertNewElement(size_t id) {
     minFreq = 1;
-    freqToElems[1].push_front(Elem{id, 1});
+    freqToElems[1].push_front(Elem{id, 1, valueFromIdFunc(id)});
     idToElem[id] = freqToElems[1].begin();
   }
 
@@ -67,12 +82,15 @@ private:
   struct Elem {
     size_t id;
     size_t freq;
+    Data value;
   };
   
   const size_t sz_;
   size_t minFreq = 0;
-  std::unordered_map<size_t, std::list<Elem>::iterator> idToElem;
+  std::unordered_map<size_t, typename std::list<Elem>::iterator> idToElem;
   std::unordered_map<size_t, std::list<Elem>> freqToElems;
+
+  std::function<Data(size_t)> valueFromIdFunc;
 };
 
 } // caches
