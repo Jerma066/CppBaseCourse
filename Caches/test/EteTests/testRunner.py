3addd7130aae31892cb72040241a7651db687f3d
Nusratillo Nosirov <nusrik.nosirov@yandex.ru>
2024-01-30 03:05:57 +0200

    1. Summary 2. -vv supporting 3. Little modifications for OOP

--- Caches/test/EteTests/testRunner.py
+++ Caches/test/EteTests/testRunner.py
@@ -1,12 +1,11 @@
 import argparse
 import os
-import pathlib
 import stat
 import subprocess
 import sys
 from pathlib import Path
 
-def is_exe(fpath):
+def is_exe(fpath) -> bool:
     """Return true if fpath is a file we have access to that is executable."""
     accessmode = os.F_OK | os.X_OK
     if os.path.exists(fpath) and os.access(fpath, accessmode) and not os.path.isdir(fpath):
@@ -14,32 +13,26 @@ def is_exe(fpath):
         ret = bool(filemode & stat.S_IXUSR or filemode & stat.S_IXGRP or filemode & stat.S_IXOTH)
         return ret
 
-def run_test(exec_file, test_path):
+def run_test(exec_path: Path, test_path: Path) -> str:
     """Run the test and return the output."""
     with open(test_path, 'r') as input_file:
         data = input_file.read()
-    result = subprocess.run([exec_file], capture_output=True, text=True, input=data)
+    result = subprocess.run([exec_path], capture_output=True, text=True, input=data)
     return result.stdout
 
-def findDiff(first, second):
-    c = 0
-    for ch in first:
-        c ^= ord(ch)
-    for ch in second:
-        c ^= ord(ch)
-    return chr(c)
- 
-def check_test_result(golden_path, test_result):
+def check_test_result(golden_path: Path, test_result: str) -> tuple:
     """Check the test result against the golden file."""
     with open(golden_path, 'r') as golden_file:
-        expected_result = golden_file.read()    
+        expected_result = golden_file.read()
     if test_result == expected_result:
-        return "PASS"
+        return True, "PASS"
     else:
-        return f"FAIL\n-----\n{test_result}\nVS\n\n{expected_result}-----\n"
+        return False, f"-----\n{test_result}\nVS\n\n{expected_result}-----\n"
 
-def find_tests_and_run(executable, directory):
+def find_tests_and_run(executable: Path, directory: Path) -> tuple:
     """Find and run tests recursively in the directory."""
+    pass_count = fail_count = unsupported_count = 0 
+    failed_tests = []
     for test_file in directory.rglob('*.dat'):
         # Checking that the test file is not in the golden folder
         if not test_file.parts[-2] == "golden":
@@ -47,11 +40,39 @@ def find_tests_and_run(executable, directory):
             golden_file = test_file.parent / "golden" / test_file.with_suffix('.ans.dat').name
             relative_path = test_file.relative_to(directory)
             if golden_file.exists():
-                test_result = run_test(str(executable), str(test_file))
-                check_result = check_test_result(str(golden_file), test_result)
-                print(f"{relative_path}: {check_result}")
+                test_result = run_test(executable, test_file)
+                result_status, result_message = check_test_result(golden_file, test_result)
+                if result_status:
+                    pass_count += 1
+                else:
+                    fail_count += 1
+                    failed_tests.append((relative_path, result_message))
             else:
+                unsupported_count += 1
                 print(f"Golden file for {relative_path} not found")
+    return pass_count, fail_count, unsupported_count, failed_tests
+
+def generate_summary(pass_count: int, 
+                     fail_count: int, 
+                     unsupported_count: int,
+                     failed_tests: list,
+                     verbose_flag: bool = False
+                     ) -> str:
+    """Generate summary of test results."""
+    summary = ""
+    if fail_count > 0:
+        summary += "\nFailed tests:\n"
+        for failed_test, message in failed_tests:
+            summary += "\n" + str(failed_test)
+            if verbose_flag:
+                summary += "\n" + message
+        summary += "\n"
+    total_tests = pass_count + fail_count + unsupported_count
+    summary += f"\nTotal tests count: {total_tests}\n"
+    summary += f"PASS:              {pass_count}\n"
+    summary += f"FAIL:              {fail_count}\n"
+    summary += f"UNSUPPORTED:       {unsupported_count}"
+    return summary
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -59,11 +80,13 @@ if __name__ == "__main__":
     # Adding optional arguments
     parser.add_argument("-e", "--executable", required=True, help = "Executable main file which will be tested")
     parser.add_argument("-t", "--test_dir", required=True, help = "Directory containing tests (and golden dir with reference results)")
+    parser.add_argument("-vv", "--VERBOSE", action="store_true", help="Enable verbose output for failed tests")
 
     # Read arguments from command line
     args = parser.parse_args()
     exec_path = Path(args.executable)
     test_root_dir = Path(args.test_dir)
+    verbose_flag = args.VERBOSE
 
     if not is_exe(exec_path):
       print(f"{exec_path} is not an executable")
@@ -76,5 +99,7 @@ if __name__ == "__main__":
     # Print checked arguments
     print("Executable main file: % s" % exec_path)
     print("Directory containing tests: % s" % test_root_dir)
-
-    find_tests_and_run(exec_path, test_root_dir)
+    
+    test_runner = find_tests_and_run(exec_path, test_root_dir)
+    summary = generate_summary(*test_runner, verbose_flag)
+    print(summary)
