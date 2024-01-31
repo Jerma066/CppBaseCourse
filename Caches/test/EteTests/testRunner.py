45b1fef4b08138384cc9d558fef3a878b2d6e177
Nusrik Nosirov <nusrik.nosirov@yandex.ru>
2024-01-31 10:00:42 +0200

    [Testing] E2E test runner script improvement
    Automated search for tests in the specified directory
    Refactored and optimized most of the functionality
    Supported -vv (--VERBOSE) input option
    Added summary output message

--- Caches/test/EteTests/testRunner.py
+++ Caches/test/EteTests/testRunner.py
@@ -1,44 +1,85 @@
 import argparse
 import os
-import pathlib
 import stat
 import subprocess
 import sys
+from pathlib import Path
 
-def isExe(fpath):
-    """ Return true if fpath is a file we have access to that is executable """
+def is_exe(fpath) -> bool:
+    """Return true if fpath is a file we have access to that is executable."""
     accessmode = os.F_OK | os.X_OK
     if os.path.exists(fpath) and os.access(fpath, accessmode) and not os.path.isdir(fpath):
         filemode = os.stat(fpath).st_mode
         ret = bool(filemode & stat.S_IXUSR or filemode & stat.S_IXGRP or filemode & stat.S_IXOTH)
         return ret
 
-def runTest(execFile, testPath):
-    inputFile = open(testPath, 'r')
-    data = "".join(inputFile.readlines())
-    inputFile.close() 
-    result = subprocess.run([execFile], capture_output=True, text=True, input=data)
+def run_test(exec_path: Path, test_path: Path) -> str:
+    """Run the test and return the output."""
+    with open(test_path, 'r') as input_file:
+        data = input_file.read()
+    result = subprocess.run([exec_path], capture_output=True, text=True, input=data)
     return result.stdout
 
-def findDiff(first, second):
-    c = 0
-    for ch in first:
-        c ^= ord(ch)
-    for ch in second:
-        c ^= ord(ch)
-    return chr(c)
- 
-def chechTestResult(goldenPath, testResult):
-    goldenFile = open(goldenPath, 'r')
-    expectedResult = "".join(goldenFile.readlines())
-    goldenFile.close()
-    checkStatus = "FAIL"
-    if testResult == expectedResult:
-      checkStatus = "PASS"
+def check_test_result(golden_path: Path, test_result: str) -> tuple:
+    """Check the test result against the golden file."""
+    with open(golden_path, 'r') as golden_file:
+        expected_result = golden_file.read()
+    if test_result == expected_result:
+        return True, "PASS"
     else:
-      checkStatus += "\n-----\n" + testResult + "\nVS\n\n" + expectedResult + "-----" + '\n'
-    return checkStatus
-    
+        return False, f"-----\n{test_result}\nVS\n\n{expected_result}-----\n"
+
+def find_tests_and_run(executable: Path, directory: Path) -> tuple:
+    """Find and run tests recursively in the directory."""
+    pass_count = fail_count = unsupported_count = 0 
+    failed_tests = []
+    unsupported_tests = []
+    for test_file in directory.rglob('*.dat'):
+        # Checking that the test file is not in the golden folder
+        if not test_file.parts[-2] == "golden":
+            # Path to golden file
+            golden_file = test_file.parent / "golden" / test_file.with_suffix('.ans.dat').name
+            relative_path = test_file.relative_to(directory)
+            if golden_file.exists():
+                test_result = run_test(executable, test_file)
+                result_status, result_message = check_test_result(golden_file, test_result)
+                if result_status:
+                    pass_count += 1
+                else:
+                    fail_count += 1
+                    failed_tests.append((relative_path, result_message))
+            else:
+                unsupported_count += 1
+                unsupported_tests.append(relative_path)
+    return pass_count, fail_count, unsupported_count, failed_tests, unsupported_tests
+
+def generate_summary(pass_count: int, 
+                     fail_count: int, 
+                     unsupported_count: int,
+                     failed_tests: list,
+                     unsupported_tests: list,
+                     verbose_flag: bool = False
+                     ) -> str:
+    """Generate summary of test results."""
+    summary = ""
+    if fail_count > 0:
+        summary += "\nFailed tests:\n"
+        for failed_test, message in failed_tests:
+            summary += "\n" + str(failed_test) 
+            if verbose_flag:
+                summary += "\n" + message
+        summary += "\n"
+    if unsupported_count > 0:
+        summary += "\nUnsupported goldens for:\n\n"
+        for unsupported_test in unsupported_tests:
+            summary += str(unsupported_test) + "\n" 
+        summary += "\n"
+    total_tests = pass_count + fail_count + unsupported_count
+    summary += f"\nTotal tests count: {total_tests}\n"
+    summary += f"PASS:              {pass_count}\n"
+    summary += f"FAIL:              {fail_count}\n"
+    summary += f"UNSUPPORTED:       {unsupported_count}"
+    return summary
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -46,97 +87,26 @@ if __name__ == "__main__":
     # Adding optional arguments
     parser.add_argument("-e", "--executable", required=True, help = "Executable main file which will be tested")
     parser.add_argument("-t", "--test_dir", required=True, help = "Directory containing tests (and golden dir with reference results)")
+    parser.add_argument("-vv", "--VERBOSE", action="store_true", help="Enable verbose output for failed tests")
 
     # Read arguments from command line
     args = parser.parse_args()
-    if not isExe(args.executable):
-      print(f"{args.executable} is not an executable")
+    exec_path = Path(args.executable)
+    test_root_dir = Path(args.test_dir)
+    verbose_flag = args.VERBOSE
+
+    if not is_exe(exec_path):
+      print(f"{exec_path} is not an executable")
       sys.exit(1)
 
-    testDir = pathlib.Path(args.test_dir)
-    if not testDir.is_dir():
-      print(f"{args.test_dir} is not a directory")
+    if not test_root_dir.is_dir():
+      print(f"{args.test_root_dir} is not a directory")
       sys.exit(1)
 
     # Print checked arguments
-    print("Executable main file: % s" % args.executable)
-    print("Directory containing tests: % s" % args.test_dir)
-
-    testResult = runTest(args.executable, args.test_dir + "test1.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test1.ans.dat", testResult)
-    print(checkResult)
-
-    testResult = runTest(args.executable, args.test_dir + "test2.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test2.ans.dat", testResult)
-    print(checkResult)
-
-
-    testResult = runTest(args.executable, args.test_dir + "test3.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test3.ans.dat", testResult)
-    print(checkResult)
-
-    testResult = runTest(args.executable, args.test_dir + "test4.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test4.ans.dat", testResult)
-    print(checkResult)
-
-    testResult = runTest(args.executable, args.test_dir + "test5.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test5.ans.dat", testResult)
-    print(checkResult)
-
-    testResult = runTest(args.executable, args.test_dir + "test6.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test6.ans.dat", testResult)
-    print(checkResult)
-
-    testResult = runTest(args.executable, args.test_dir + "test7.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test7.ans.dat", testResult)
-    print(checkResult)
-
-    testResult = runTest(args.executable, args.test_dir + "test8.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test8.ans.dat", testResult)
-    print(checkResult)
-
-    testResult = runTest(args.executable, args.test_dir + "test9.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test9.ans.dat", testResult)
-    print(checkResult)
-
-    testResult = runTest(args.executable, args.test_dir + "test10.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test10.ans.dat", testResult)
-    print(checkResult)
-
-    testResult = runTest(args.executable, args.test_dir + "test11.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test11.ans.dat", testResult)
-    print(checkResult)
-
-    testResult = runTest(args.executable, args.test_dir + "test12.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test12.ans.dat", testResult)
-    print(checkResult)
-
-    testResult = runTest(args.executable, args.test_dir + "test13.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test13.ans.dat", testResult)
-    print(checkResult)
-
-    testResult = runTest(args.executable, args.test_dir + "test14.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test14.ans.dat", testResult)
-    print(checkResult)
-
-
-    testResult = runTest(args.executable, args.test_dir + "test15.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test15.ans.dat", testResult)
-    print(checkResult)
-
-
-    testResult = runTest(args.executable, args.test_dir + "test16.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/test16.ans.dat", testResult)
-    print(checkResult)
-
-    testResult = runTest(args.executable, args.test_dir + "vki_005.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/vki_005.ans.dat", testResult)
-    print(checkResult)
-
-    testResult = runTest(args.executable, args.test_dir + "vki_20-500.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/vki_20-500.ans.dat", testResult)
-    print(checkResult)
-
-    testResult = runTest(args.executable, args.test_dir + "vki_011.dat")
-    checkResult = chechTestResult(args.test_dir + "golden/vki_011.ans.dat", testResult)
-    print(checkResult)
+    print("Executable main file: % s" % exec_path)
+    print("Directory containing tests: % s" % test_root_dir)
+    
+    test_runner = find_tests_and_run(exec_path, test_root_dir)
+    summary = generate_summary(*test_runner, verbose_flag)
+    print(summary)
